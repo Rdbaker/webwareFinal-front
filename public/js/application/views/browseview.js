b0269dc2ed560d622ff42ffd59592ba28533b37a
strahman218 <strahman@wpi.edu>
2014-12-15 08:46:51 -0500

    add dropdown menu for browse stocks

--- public/js/application/views/browseview.js
+++ public/js/application/views/browseview.js
@@ -11,38 +11,60 @@
       (function (_this) {
         $.get('/browse-view', function (data) {
           $(_this.el).html(data);
+          _this.makeTableFromGames([{id: 100, name: 'test', price: 100}]);
 
-          _this.makeBrowseStocksTable([{name: 'test', val: 100}]);
-
+          // close the new game form
+          $("#add-stock-form", $(_this.el)).hide();
         });
       })(this);
     },
 
+        // TODO: remove once finished testing
 
-    //add stockname, price table and sort by most popular
-    makeBrowseStocksTable: function (browseStocks) {
-      var tableBody = $('tableBody', $(browseStocks));
+    //add id, stockname, price table and sort by most popular
+     makeBrowseStocksTable: function (browseStocks) {
+     var tableBody = $('tableBody', $("#browseStocks"));
 
-      var td1, td2, tr;
-      // make a new row for each stock
-      for (var i = 0; i < browseStocks.length; i++) {
-        td1 = document.createElement('td');
-        td2 = document.createElement('td');
-        tr = document.createElement('tr');
-        td1.innerText = browseStocks[i].name;
-        td2.innerText = browseStocks[i].val;
+     var td1, td2, td3, tr;
+     // make a new row for each stock
+     for (var i = 0; i < browseStocks.length; i++) {
+     td1 = document.createElement('td');
+     td2 = document.createElement('td');
+     td3 = document.createElement('td');
+     tr = document.createElement('tr');
+     td1.innerText = browseStocks[i].id;
+     td2.innerText = browseStocks[i].name;
+     td3.innerText = browseStocks[i].price;
 
-        // add it to the table
-        tr.appendChild(td1);
-        tr.appendChild(td2);
-        tableBody[0].appendChild(tr);
+     // add it to the table
+     tr.appendChild(td1);
+     tr.appendChild(td2);
+     tr.appendChild(td3);
+     tableBody[0].appendChild(tr);
+  }
 
 
-      }
     },
 
+    // open the add stock form
+    addStock: function () {
+      // open the form
+      $("#add-stock-form", $(this.el)).slideDown();
+    },
 
-    // TODO: add search bar (can only search symbol (abbr.)
+    // close the add stock form and clear the inputs
+    cancelGame: function () {
+      var form = $("#add-stock-form", $(this.el));
+      // close the form
+      form.slideUp();
+
+      // clear the form
+      $('input', form).val("");
+    },
+
+    retrieveStocksData: function(stockId){
+
+    },
     // todo: when user clicks on a a stock,
     // have the div slide down and let them have the option
     // to add it to their portfolio for a specific game
@@ -51,6 +73,9 @@
 
     // set up the events
     events: {
+      'click #add-stock-btn': 'addStock',
+      'click #cancel': 'cancel',
+      'click #submit' : 'submit',
       'click #browseStocks > tableBody > tr': 'getStockInfo'
     },
 
