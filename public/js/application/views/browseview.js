f901e412b2d28d3913238ae349e441f8ec83da3d
strahman218 <strahman@wpi.edu>
2014-12-15 03:45:16 -0500

    add browse table

--- public/js/application/views/browseview.js
+++ public/js/application/views/browseview.js
@@ -1,44 +1,57 @@
 (function(window, document, $) {
   // make a backbone view
-  var BrowseView = Backbone.View.extend({
+  var BrowseView;
+  BrowseView = Backbone.View.extend({
     // the tagname for this view's element
     tagname: "div",
 
     // what to do on initialization
-    initialize: function() {
+    initialize: function () {
       // a little trick to use proper scoping
-      (function(_this) {
-        $.get('/browse-view', function(data) {
+      (function (_this) {
+        $.get('/browse-view', function (data) {
           $(_this.el).html(data);
+
+          _this.makeBrowseStocksTable([{name: 'test', val: 100}]);
+
         });
       })(this);
     },
 
+
     //add stockname, price table and sort by most popular
-    makeBrowseStocksTable: function(browseStocks){
+    makeBrowseStocksTable: function (browseStocks) {
       var tableBody = $('tableBody', $(browseStocks));
 
-      var td, tr;
+      var td1, td2, tr;
       // make a new row for each stock
-      for (var i=0; i<browseStocks.length; i++){
-        td = document.createElement('td');
+      for (var i = 0; i < browseStocks.length; i++) {
+        td1 = document.createElement('td');
+        td2 = document.createElement('td');
         tr = document.createElement('tr');
-        td.innerText(browseStocks[i]);
+        td1.innerText = browseStocks[i].name;
+        td2.innerText = browseStocks[i].val;
 
         // add it to the table
-        tr.appendChild(td);
+        tr.appendChild(td1);
+        tr.appendChild(td2);
         tableBody[0].appendChild(tr);
+
+
       }
     },
 
-    /* TODO: add search bar
-    // TODO: when user clicks on a a stock,
+
+    // TODO: add search bar (can only search symbol (abbr.)
+    // todo: when user clicks on a a stock,
     // have the div slide down and let them have the option
     // to add it to their portfolio for a specific game
-    (have a drop down of the games they're currently in)*/
+    //(have a drop down of the games they're currently in)*/
+
 
     // set up the events
     events: {
+      'click #browseStocks > tableBody > tr': 'getStockInfo'
     },
 
 
