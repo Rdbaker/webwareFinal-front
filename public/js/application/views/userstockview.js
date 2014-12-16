07d9f5fee6b2d35b9d460e026ae9a7eb2fed8026
Mike Calder <mrcalder@wpi.edu>
2014-12-16 04:48:45 -0500

    Add code for buy/sell stock.

--- public/js/application/views/userstockview.js
+++ public/js/application/views/userstockview.js
@@ -15,7 +15,108 @@
     },
 
     buyOrSell: function(e) {
-      console.log('hes');
+        // get the game id
+      var gameId = $('.warning')[0].id;
+
+      if ($(e.currentTarget).hasClass('sell')) {
+
+        (function(_this){
+          new Application.Services.APIRequestService({
+            // type of request
+            'type': "POST",
+            // data for the POST
+            'data':
+              {
+                'authToken' : window.authToken,
+                'gameId'    : gameId,
+                'stockName' : e.currentTarget.parentElement.parentElement.childNodes[0].innerText,
+                'amount'    : e.currentTarget.parentElement.childNodes[1].value
+              },
+            // endpoint for the API to hit
+            'uri': "/stocks/sell",
+            // callback function for the request
+            'callback': function (data) {
+              var stocks = Number(e.currentTarget.parentElement.parentElement.childNodes[1].innerText);
+              stocks -= Number(e.currentTarget.parentElement.childNodes[1].value);
+              e.currentTarget.parentElement.parentElement.childNodes[1].innerText = stocks;
+
+              var moneyChange = Number(e.currentTarget.parentElement.childNodes[1].value) * Number(e.currentTarget.parentElement.parentElement.childNodes[2].innerText);
+            
+              (function(_this){
+                new Application.Services.APIRequestService({
+                  // type of request
+                  'type': "POST",
+                  // data for the POST
+                  'data':
+                    {
+                      'authToken' : window.authToken,
+                      'gameId'    : gameId,
+                      'amount'    : Number($('.warning')[0].childNodes[1].innerText) + moneyChange
+                    },
+                  // endpoint for the API to hit
+                  'uri': "/games/remainingMoney",
+                  // callback function for the request
+                  'callback': function (data) {
+                  }
+                });
+              })(this);
+
+              $('.warning')[0].childNodes[1].innerText = (Number($('.warning')[0].childNodes[1].innerText) + moneyChange).toFixed(2);
+              e.currentTarget.parentElement.childNodes[1].value = '';
+            }
+          });
+        })(this);
+
+      } else {
+
+        (function(_this){
+          new Application.Services.APIRequestService({
+            // type of request
+            'type': "POST",
+            // data for the POST
+            'data':
+              {
+                'authToken' : window.authToken,
+                'gameId'    : gameId,
+                'stockName' : e.currentTarget.parentElement.parentElement.childNodes[0].innerText,
+                'amount'    : e.currentTarget.parentElement.childNodes[1].value
+              },
+            // endpoint for the API to hit
+            'uri': "/stocks/buy",
+            // callback function for the request
+            'callback': function (data) {
+              var stocks = Number(e.currentTarget.parentElement.parentElement.childNodes[1].innerText);
+              stocks += Number(e.currentTarget.parentElement.childNodes[1].value);
+              e.currentTarget.parentElement.parentElement.childNodes[1].innerText = stocks;
+
+              var moneyChange = Number(e.currentTarget.parentElement.childNodes[1].value) * Number(e.currentTarget.parentElement.parentElement.childNodes[2].innerText);
+
+              (function(_this){
+                new Application.Services.APIRequestService({
+                  // type of request
+                  'type': "POST",
+                  // data for the POST
+                  'data':
+                    {
+                      'authToken' : window.authToken,
+                      'gameId'    : gameId,
+                      'amount'    : Number($('.warning')[0].childNodes[1].innerText) - moneyChange
+                    },
+                  // endpoint for the API to hit
+                  'uri': "/games/remainingMoney",
+                  // callback function for the request
+                  'callback': function (data) {
+                  }
+                });
+              })(this);
+
+              $('.warning')[0].childNodes[1].innerText = (Number($('.warning')[0].childNodes[1].innerText) - moneyChange).toFixed(2);
+              e.currentTarget.parentElement.childNodes[1].value = '';
+            }
+          });
+        })(this);
+
+      }
     },
 
     // set up the events
